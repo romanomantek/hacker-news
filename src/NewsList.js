a9484c47e4b1ea254dddbbc93a0d728d29a31d6c
Nettester <github@gluch.eu>
2022-06-27 20:37:15 +0200

    file deletes / small edits

--- src/NewsList.js
+++ src/NewsList.js
@@ -1,27 +1,29 @@
-import hackernews from './hackernews';
+import React from 'react';
+import hackerNews from './hackerNews';
 
 export default function NewsList(){
     //Fragen: Warum wird hier in der Konsole das Array zwei Mal angezeigt?
     //Wie debugge ich?
-    //console.log(hackernews.hits[0].title);
-    //console.log(hackernews.title);
-    const newsListLength = hackernews.hits.length;
+    //console.log(hackerNews.hits[0].title);
+    //console.log(hackerNews.title);
+    //const newsListLength = hackerNews.hits.length;
     //console.log(newsListLength);
-    const listElement = () =>{
+    /* const listElement = () =>{
         let list = "";
         for(let i=0; i<newsListLength; i++){
-            //console.log(hackernews.hits[i].title);
-            const title = hackernews.hits[i].title;
+            //console.log(hackerNews.hits[i].title);
+            const title = hackerNews.hits[i].title;
             list += `<li>${title}</li>`;
         }
         return list;
-    };
+    }; */
+    const posts = hackerNews.hits
+    console.log(posts);
 
     return(
         <>
-            <div className="Title">
-                <ul>{listElement()}
-                </ul>
+            <div className="Titles">
+                {posts.map((e) => <p key={e.objectID}>{e.title}</p>)}
             </div>
             
         </>
