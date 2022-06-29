4ead5f01b84f76331af423c6b13bc6dbb95cd4ee
Melis Augsburger <melis.augsburger@outlook.com>
2022-06-29 16:33:08 +0200

    search function added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NewsList from './NewsList';
 
 export default function Navbar(){
-    const search = () =>{
+    const [news, setNews] = useState([]);
+    const [input, setInput] = useState('');
 
+    useEffect(() => {
+        fetch(`http://hn.algolia.com/api/v1/search?query=${input}`)
+        .then((response) => response.json())
+        .then((data) => setNews(data.hits))
+        .catch((error) => console.log(`Oops, something went wrong. ${error}`));
+
+    }, [input])
+   
+    const search = (event) =>{
+        const input = event.target.value;
+        setInput(input);
     };
 
     return(
@@ -10,5 +23,6 @@ export default function Navbar(){
             <input type="text" onChange={search}></input>
             <button>Search</button>
         </header>
+        
     );
 }
