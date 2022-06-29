b34c46dc86630188045d0c5884a3bb5ee3432d0c
Nettester <github@gluch.eu>
2022-06-29 11:36:50 +0200

    simplified code

--- src/NewsList.js
+++ src/NewsList.js
@@ -9,9 +9,10 @@ export default function NewsList(){
     useEffect(() => {
         const apiUrl = "http://hn.algolia.com/api/v1/search_by_date?tags=story";
         fetch(apiUrl)
-          .then((res) => res.json())
-          .then((news) => { setPosts(news.hits);});
-      }, [setPosts]);
+          .then((response) => response.json())
+          .then((resJson) => setPosts(resJson.hits))
+          .catch((err) => alert(`API load failed (${err})`));
+    },[]);
     //const posts = hackerNews.hits;
     const calcDays = (created_at) => {
         const created = new Date(created_at);
@@ -23,13 +24,7 @@ export default function NewsList(){
     // wrap information of an object in p-tag
     const wrapPost = (e) => {
         if (e.title) {
-            let hostname;
-            // check if properties not empty
-            try {
-                hostname = new URL(e.url).hostname 
-            } catch {
-                hostname = "...";
-            }
+            const hostname = e.url ? new URL(e.url).hostname : "";
             // return wrapped information in a div
             return (
                 <div className="newsRow" key={e.objectID}>
