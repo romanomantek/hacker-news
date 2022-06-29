b132cc7607f27d0c57dd093072b95faea642d324
Nettester <github@gluch.eu>
2022-06-29 02:14:36 +0200

    add edit newsRow

--- src/NewsList.js
+++ src/NewsList.js
@@ -2,32 +2,43 @@ import React, { useState } from 'react';
 import hackerNews from './hackerNews';
 
 export default function NewsList(){
-    //Fragen: Warum wird hier in der Konsole das Array zwei Mal angezeigt?
-    //Wie debugge ich?
-    //console.log(hackerNews.hits[0].title);
-    //console.log(hackerNews.title);
-    //const newsListLength = hackerNews.hits.length;
-    //console.log(newsListLength);
-    /* const listElement = () =>{
-        let list = "";
-        for(let i=0; i<newsListLength; i++){
-            //console.log(hackerNews.hits[i].title);
-            const title = hackerNews.hits[i].title;
-            list += `<li>${title}</li>`;
-        }
-        return list;
-    }; */
-    // useState for variable input of API
+     // useState for variable input of API
     const [posts, setPosts] = useState(hackerNews.hits);
-    
-    console.log(posts);
+    //const posts = hackerNews.hits;
+    const calcDays = (created_at) => {
+        const created = new Date(created_at);
+        const today = new Date();
+        const oneDay = 1000 * 60 * 60 * 24;
+        const diffInTime = today.getTime() - created.getTime();
+        return  Math.round(diffInTime / oneDay);
+    }
     // wrap information of an object in p-tag
-    const wrapPost = ({objectID, title}) => <p key={objectID}>{title}</p>
+    const wrapPost = (e) => {
+        let hostname;
+        // check if properties not empty
+        try {
+            hostname = new URL(e.url).hostname 
+        } catch {
+            hostname = "?";
+        }
+        // return wrapped information in a div
+        return (
+            <div className="newsRow" key={e.objectID}>
+                <p className="news">
+                    <strong className="newsTitle"> {e.title} - </strong>
+                    <a className="newsUrl" href={e.url}> {hostname} </a>
+                    <br />
+                    <small className="newsAuthor"> Author: {e.author} </small>
+                    <small className="newsOld"> Exists: {calcDays(e.created_at)} days</small>
+                </p>
+            </div>
+        )
+    }
 
     return(
         <>
-            <div className="Titles">
-                {/* {posts.map((e) => <p key={e.objectID}>{e.title}</p>) */}
+            <div className="titles">
+                {/* {posts.map((e) => <p key={e.objectID}>{e.title}</p>)} */}
                 {posts.map(wrapPost)}
             </div>
             
