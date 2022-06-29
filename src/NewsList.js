80852ef3b0d597dd8980f8234e35e1c9aec5af19
Nettester <github@gluch.eu>
2022-06-29 12:30:22 +0200

    step back

--- src/NewsList.js
+++ src/NewsList.js
@@ -3,19 +3,10 @@ import hackerNews from './hackerNews';
 
 export default function NewsList(){
      // useState for variable input of API
+    const [posts, setPosts] = useState(hackerNews.hits);
     
-    const [posts, setPosts] = useState([hackerNews.hits]);
-    
-    //const posts = hackerNews.hits;
-    const calcDays = (created_at) => {
-        const created = new Date(created_at);
-        const today = new Date();
-        const oneDay = 1000 * 60 * 60 * 24;
-        const diffInTime = today.getTime() - created.getTime();
-        return  Math.round(diffInTime / oneDay);
-    }
     // wrap information of an object in p-tag
-    const wrapPost = (e) => <p key={e.objectID}>{e.title}</p>
+    const wrapPost = ({objectID, title}) => <p key={objectID}>{title}</p>
     
     return(
             <div className="titles">
