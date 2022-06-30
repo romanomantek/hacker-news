8814d93bcfceb7ba69ccb3ca25203cacf9629cda
Melis Augsburger <melis.augsburger@outlook.com>
2022-06-30 18:08:57 +0200

    add radiobutton

--- src/apiFetcher.js
+++ src/apiFetcher.js
@@ -2,6 +2,7 @@ const properties = {
     apiInitUrl : "http://hn.algolia.com/api/v1/search_by_date?tags=story",
     apiSearchUrl : "http://hn.algolia.com/api/v1/search",
     apiUserUrl : "http://hn.algolia.com/api/v1/users/",
+    apiSearchByAuthorUrl : "http://hn.algolia.com/api/v1/search_by_date?tags=author_",
 }
 
 export async function getNews(url=properties.apiInitUrl) {
@@ -11,5 +12,15 @@ export async function getNews(url=properties.apiInitUrl) {
         return resJson.hits;
     } catch (err) {
         return `API load failed (${err})`;
+    }
+}
+
+export async function getNewsByAuthor(author) {
+    try {
+        const response = await fetch(properties.apiSearchByAuthorUrl+author);
+        const resJson = await response.json();
+        return resJson.hits;
+    } catch (error) {
+        return `Author biibbbiiibibi melis was here(${error})`;
     }
 }
